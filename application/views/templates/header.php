6df55e96c9f0f9103595888cbc63ebd42f97c6c0
Brandon van Zanten <brandonvanzanten@gmail.com>
2019-02-15 23:17:39 +0100

    initial commit, configured codeigniter

---
+++ application/views/templates/header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+<head>
+    <!-- Meta Tags -->
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <!-- Title -->
+    <title>To-Do-List | Login</title>
+
+    <!-- CSS Libraries -->
+    <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css">
+
+    <!-- Custom CSS -->
+    <link rel="stylesheet" type="text/css" href="<? echo base_url();?>assets/css/style.css">
+
+    <!-- JS Libraries -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.7.2/angular.min.js"></script>
+
+    <!-- Custom JS -->
+    <script src="<? echo base_url(); ?>assets/js/dashboard.js"></script>
+    <script src="<? echo base_url(); ?>assets/js/login.js"></script>
+
+</head>
+
+<body>
---
+++ assets/js/dashboard.js
@@ -0,0 +1,6 @@
+var baseUrl = "http://localhost/to-do-list/";
+var app = angular.module('myToDo', []);
+
+app.controller('DashboardController', function($scope, $http) {
+
+}
